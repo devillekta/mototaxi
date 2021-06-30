e60f5b3612105af3508fd88eb05fcf2cb445b632
Diego Deville <devillekta@gmail.com>
2021-06-30 11:18:09 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mototaxi',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
